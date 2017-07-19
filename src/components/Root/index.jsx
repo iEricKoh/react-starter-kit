ae3571c7a6ff5c8368228a8165203dab6a3d807b
Eric Koh <erickoh.dev@gmail.com>
2017-07-19 23:32:28 +0800

    fixes reducer hotreload

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,21 +1,14 @@
-import React, { Component }             from 'react'
-import { BrowserRouter as Router }      from 'react-router-dom'
-import App                              from '../App'
-import { createStore, applyMiddleware } from 'redux'
-import thunk                            from 'redux-thunk'
-import { Provider }                     from 'react-redux'
-import rootReducer                      from '../../reducers'
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-)
+import React, { Component }        from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App                         from '../App'
+import { Provider }                from 'react-redux'
+import createStore                 from './createStore'
 
 class Root extends Component {
   render() {
     return (
       <Router>
-        <Provider store={store}>
+        <Provider store={createStore()}>
           <App />
         </Provider>
       </Router>
